83e1592cbb076f24805e936d9ea86d7b4c4a6c23
Zack M. Davis <code@zackmdavis.net>
2014-02-16 16:52:13 -0800

    slightly better modularity?
    But really, it's important to remember that filling the grid by
    placing actual words is not sufficient; you also have to make sure
    that your placements don't form nonsense "words" in the other
    direction. I expect this will take some intelligence in selecting
    words to place; "rand-nth" of naive matches isn't cutting it.
    
    Also, one test run produced an amusing coincidence:
    
    (:P :O :T :S)
    (:L :I :S :P)  ;; (!!)
    (nil nil :A :A)
    (nil nil :R :M)

--- core.clj
+++ core.clj
@@ -16,8 +16,6 @@
 (defn lookup [grid position]
   ((grid (first position)) (second position)))
 
-(def demo_grid (empty_grid 4))
-
 (defn word_to_seq [word]
   (map #(keyword (clojure.string/upper-case (str %))) word))
 
@@ -36,7 +34,8 @@
 (defn display_grid [grid]
   (let [n (count grid)]
     (doseq [i (range n)]
-      (println (get_constraints grid i :across)))))
+      (println (get_constraints grid i :across))))
+  (println))
 
 (defn write_word [grid word i orientation]
   (let [n (count grid)]
@@ -52,15 +51,32 @@
     (filter fit? words)))
 
 (defn solve_word [words grid i orientation]
-  (write_word grid
-              (rand-nth (search words
-                                (get_constraints grid i orientation)))
-                        i
-                        orientation))
+  (let [results (search words
+                        (get_constraints grid i orientation))]
+    (if (seq results)
+      (do
+        (write_word grid (rand-nth results) i orientation)
+        true)
+      false)))
 
-(write_word demo_grid [:B :I :T :S] 0 :across)
-(solve_word dictionary demo_grid 0 :down)
-(solve_word dictionary demo_grid 1 :across)
-(solve_word dictionary demo_grid 1 :down)
+(defn solve [words grid prompts]
+  (let [prompt (first prompts)]
+    (if (solve_word words grid (first prompt) (second prompt))
+      (do 
+        (display_grid grid)
+        (solve words grid (rest prompts)))
+      (println "dead end or solved"))))
 
-(display_grid demo_grid)
+(def demo_grid (empty_grid 4))
+
+(def demo_prompts
+  (shuffle
+   [[0 :across]
+    [0 :down]
+    [1 :across]
+    [1 :down]
+    [2 :across]
+    [2 :down]
+    [3 :down]]))
+
+(solve dictionary demo_grid demo_prompts)
